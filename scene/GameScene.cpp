47f44dc53515bd5660eaee182294abe49bdc0709
Jun_T <taka.jun.nelu@icloud.com>
2022-04-11 16:14:57 +0900

    01_02_確認課題

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -7,11 +7,11 @@ using namespace DirectX;
 GameScene::GameScene() {}
 
 GameScene::~GameScene() { 
-	/*
+	
 	delete sprite_;
 
 	delete model_;
-	*/
+	
 }
 
 void GameScene::Initialize() {
@@ -21,15 +21,15 @@ void GameScene::Initialize() {
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
 
-	/*
+	
 	//ファイル名を指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("Xion.png");
+	textureSpriteHandle_ = TextureManager::Load("Xion.png");
 	//スプライトの生成
-	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+	sprite_ = Sprite::Create(textureSpriteHandle_, {100, 50});
 
 
 	//ファイルを指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("Xion.png");
+	textureModelHandle_ = TextureManager::Load("Xion.png");
 	//3Dモデルの生成
 	model_ = Model::Create();
 
@@ -37,22 +37,24 @@ void GameScene::Initialize() {
 	worldTransform_.Initialize();
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
-	*/
+	
 
-	/*
+	
 	//サウンドデータの読み込み
 	soundDataHandle_ = audio_->LoadWave("fanfare.wav");
 	
+	/*
 		//音声再生
 		audio_->PlayWave(soundDataHandle_);
+	*/
 
 	//音声再生(ループ)
 	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
-	*/
+	
 }
 
 void GameScene::Update() {
-	/*
+	
 	//スプライトの今の座標を取得
 	XMFLOAT2 position = sprite_->GetPosition();
 	//座標を{2,0}移動
@@ -60,21 +62,22 @@ void GameScene::Update() {
 	position.y += 1.0f;
 	//移動した座標をスプライトに反映
 	sprite_->SetPosition(position);
-	*/
+	
 
-	/*
+	
 	if (input_->TriggerKey(DIK_SPACE)) {
 	//音声停止
 		audio_->StopWave(voiceHandle_);
 	}
-	*/
-
+	
+	/*
 	//デバッグテキストの表示 (表示内容,X座標,Y座標,倍率)日本語は非対応
 	debugText_->Print("Kaizokuou ni oreha naru.", 50, 50, 1.0f);
 
 	//書式指定付き表示
 	debugText_->SetPos(50, 70);
 	debugText_->Printf("year:%d", 2001);
+	*/
 
 	//変数の値をインクリメント
 	value_++;
@@ -82,7 +85,7 @@ void GameScene::Update() {
 	std::string strDebug = std::string("Value:") + 
 		std::to_string(value_);
 	//デバッグテキストの表示
-	debugText_->Print(strDebug, 50, 90, 1.0f);
+	debugText_->Print(strDebug, 50, 50, 1.0f);
 }
 
 void GameScene::Draw() {
@@ -98,9 +101,9 @@ void GameScene::Draw() {
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
 
-	/*
+
 	sprite_->Draw();
-	*/
+	
 
 	// スプライト描画後処理
 	Sprite::PostDraw();
@@ -116,9 +119,9 @@ void GameScene::Draw() {
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
 
-	/*
-	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
-	*/
+	
+	model_->Draw(worldTransform_, viewProjection_, textureModelHandle_);
+	
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
