d5df815f9291218da57f5d4ac63494b6728c4f1d
Jun_T <taka.jun.nelu@icloud.com>
2022-04-18 14:50:10 +0900

    いらないところの削除(初期化) モデルのスケール変更

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -7,8 +7,6 @@ using namespace DirectX;
 GameScene::GameScene() {}
 
 GameScene::~GameScene() { 
-	
-	delete sprite_;
 
 	delete model_;
 	
@@ -21,71 +19,24 @@ void GameScene::Initialize() {
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
 
-	
-	//ファイル名を指定してテクスチャを読み込む
-	textureSpriteHandle_ = TextureManager::Load("Xion.png");
-	//スプライトの生成
-	sprite_ = Sprite::Create(textureSpriteHandle_, {100, 50});
-
-
 	//ファイルを指定してテクスチャを読み込む
 	textureModelHandle_ = TextureManager::Load("Xion.png");
 	//3Dモデルの生成
 	model_ = Model::Create();
 
+	//X,Y,Z 方向のスケーリングを設定
+	worldTransform_.scale_ = {5.0f, 1.0f, 1.0f};
+
 	//ワールドトランスフォームの初期化
 	worldTransform_.Initialize();
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
-	
-
-	
-	//サウンドデータの読み込み
-	soundDataHandle_ = audio_->LoadWave("fanfare.wav");
-	
-	/*
-		//音声再生
-		audio_->PlayWave(soundDataHandle_);
-	*/
-
-	//音声再生(ループ)
-	voiceHandle_ = audio_->PlayWave(soundDataHandle_, true);
-	
 }
 
 void GameScene::Update() {
-	
-	//スプライトの今の座標を取得
-	XMFLOAT2 position = sprite_->GetPosition();
-	//座標を{2,0}移動
-	position.x += 2.0f;
-	position.y += 1.0f;
-	//移動した座標をスプライトに反映
-	sprite_->SetPosition(position);
-	
 
-	
-	if (input_->TriggerKey(DIK_SPACE)) {
-	//音声停止
-		audio_->StopWave(voiceHandle_);
-	}
-	
-	/*
-	//デバッグテキストの表示 (表示内容,X座標,Y座標,倍率)日本語は非対応
-	debugText_->Print("Kaizokuou ni oreha naru.", 50, 50, 1.0f);
 
-	//書式指定付き表示
-	debugText_->SetPos(50, 70);
-	debugText_->Printf("year:%d", 2001);
-	*/
 
-	//変数の値をインクリメント
-	value_++;
-	//値を含んだ文字列
-	std::string strDebug = std::string("Value:") + 
-		std::to_string(value_);
-	//デバッグテキストの表示
-	debugText_->Print(strDebug, 50, 50, 1.0f);
 }
 
 void GameScene::Draw() {
@@ -100,9 +51,6 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに背景スプライトの描画処理を追加できる
 	/// </summary>
-
-
-	sprite_->Draw();
 	
 
 	// スプライト描画後処理
