1e9e1c23ef412e2e1c8765b023731b82e00bdc59
Jun_T <taka.jun.nelu@icloud.com>
2022-04-25 14:51:07 +0900

    02_03_開始状態に変更

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -50,107 +50,99 @@ void GameScene::Initialize() {
 		worldTransform_[i].Initialize();
 	}
 
-	//カメラ視点座標を設定
-	viewProjection_.eye = {0, 0, -10};
-
-	//カメラ注視座標を設定
-	viewProjection_.target = {10, 0, 0};
-
-	//カメラ上方向ベクトルを設定(右上45度指定)
-	viewProjection_.up = {cosf(XM_PI / 4.0f), sinf(XM_PI / 4.0f), 0.0f};
-
 	//ビュープロジェクションの初期化
 	viewProjection_.Initialize();
 }
 
 void GameScene::Update() {
+
 #pragma region //視点移動処理//
 
-	
 	//視点の移動ベクトル
-	XMFLOAT3 move = {0, 0, 0};
+	//XMFLOAT3 move = {0, 0, 0};
 
-	//視点の移動速度
-	const float kEyeSpeed = 0.2f;
+	////視点の移動速度
+	//const float kEyeSpeed = 0.2f;
 
-	//押した方向で移動ベクトルを変更
-	if (input_->PushKey(DIK_W)) {
-		move = {0, 0, kEyeSpeed};
-	} else if (input_->PushKey(DIK_S)) {
-		move = {0, 0, -kEyeSpeed};
-	}
+	////押した方向で移動ベクトルを変更
+	//if (input_->PushKey(DIK_W)) {
+	//	move = {0, 0, kEyeSpeed};
+	//} else if (input_->PushKey(DIK_S)) {
+	//	move = {0, 0, -kEyeSpeed};
+	//}
 
-	//視点移動(ベクトルの加算)
-	viewProjection_.eye.x += move.x;
-	viewProjection_.eye.y += move.y;
-	viewProjection_.eye.z += move.z;
+	////視点移動(ベクトルの加算)
+	//viewProjection_.eye.x += move.x;
+	//viewProjection_.eye.y += move.y;
+	//viewProjection_.eye.z += move.z;
 
-	//行列の再計算
-	viewProjection_.UpdateMatrix();
+	////行列の再計算
+	//viewProjection_.UpdateMatrix();
 
-	//デバッグ用表示
-	debugText_->SetPos(50, 50);
-	debugText_->Printf(
-	  "eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);
+	////デバッグ用表示
+	//debugText_->SetPos(50, 50);
+	//debugText_->Printf(
+	//  "eye:(%f,%f,%f)", viewProjection_.eye.x, viewProjection_.eye.y, viewProjection_.eye.z);
+	//
 
 
 #pragma endregion
 
 #pragma region //注視点移動処理//
 
-	
-	//注視点の移動ベクトル
-	move = {0, 0, 0};
+	//
+	////注視点の移動ベクトル
+	//move = {0, 0, 0};
 
-	//注視点の移動速度
-	const float kTargetSpeed = 0.2f;
+	////注視点の移動速度
+	//const float kTargetSpeed = 0.2f;
 
-	//押した方向で移動ベクトルを変更
-	if (input_->PushKey(DIK_LEFT)) {
-		move = {-kTargetSpeed, 0, 0};
-	} else if (input_->PushKey(DIK_RIGHT)) {
-		move = {kTargetSpeed, 0, 0};
-	}
+	////押した方向で移動ベクトルを変更
+	//if (input_->PushKey(DIK_LEFT)) {
+	//	move = {-kTargetSpeed, 0, 0};
+	//} else if (input_->PushKey(DIK_RIGHT)) {
+	//	move = {kTargetSpeed, 0, 0};
+	//}
 
-	//注視点移動(ベクトルの加算)
-	viewProjection_.target.x += move.x;
-	viewProjection_.target.y += move.y;
-	viewProjection_.target.z += move.z;
+	////注視点移動(ベクトルの加算)
+	//viewProjection_.target.x += move.x;
+	//viewProjection_.target.y += move.y;
+	//viewProjection_.target.z += move.z;
 
-	//行列の再計算
-	viewProjection_.UpdateMatrix();
+	////行列の再計算
+	//viewProjection_.UpdateMatrix();
 
-	//デバッグ用表示
-	debugText_->SetPos(50, 70);
-	debugText_->Printf(
-	  "target:(%f,%f,%f)", viewProjection_.target.x, viewProjection_.target.y,
-	  viewProjection_.target.z);
+	////デバッグ用表示
+	//debugText_->SetPos(50, 70);
+	//debugText_->Printf(
+	//  "target:(%f,%f,%f)", viewProjection_.target.x, viewProjection_.target.y,
+	//  viewProjection_.target.z);
 
 
 #pragma endregion
 
 #pragma region //上方向回転処理//
 
-	//上方向の回転速度(ラジアン/Frame)
-	const float kUpRotSpeed = 0.05f;
+	////上方向の回転速度(ラジアン/Frame)
+	//const float kUpRotSpeed = 0.05f;
 
-	//押した方向で移動ベクトルを変更
-	if (input_->PushKey(DIK_SPACE)) {
-		viewAngle += kUpRotSpeed;
-		//2πを超えたら0に戻す
-		viewAngle = fmodf(viewAngle, XM_2PI);
-	}
+	////押した方向で移動ベクトルを変更
+	//if (input_->PushKey(DIK_SPACE)) {
+	//	viewAngle += kUpRotSpeed;
+	//	//2πを超えたら0に戻す
+	//	viewAngle = fmodf(viewAngle, XM_2PI);
+	//}
 
-	//上方向ベクトルを計算(半径1の円周上の座標)
-	viewProjection_.up = {cosf(viewAngle), sinf(viewAngle), 0.0f};
+	////上方向ベクトルを計算(半径1の円周上の座標)
+	//viewProjection_.up = {cosf(viewAngle), sinf(viewAngle), 0.0f};
 
-	//行列の再計算
-	viewProjection_.UpdateMatrix();
+	////行列の再計算
+	//viewProjection_.UpdateMatrix();
 
-	//デバッグ用表示
-	debugText_->SetPos(50, 90);
-	debugText_->Printf(
-	  "up:(%f,%f,%f)", viewProjection_.up.x, viewProjection_.up.y, viewProjection_.up.z);
+	////デバッグ用表示
+	//debugText_->SetPos(50, 90);
+	//debugText_->Printf(
+	//  "up:(%f,%f,%f)", viewProjection_.up.x, viewProjection_.up.y, viewProjection_.up.z);
 
 #pragma endregion
 }
